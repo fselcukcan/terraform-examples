521c5bc816329c0deba93d718823f6fdba28db47
Faruk Selçuk Can <selcuk_can@epam.com>
2022-08-05 22:00:42 +0200

    extracted module to a git repo and can use remotely

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "work_queue" {
-    source = "./sqs-with-backoff"
+    source = "github.com/fselcukcan/terraform-sqs-with-backoff.git"
     queue_name = "work-queue"
     env = var.env
 }
 
 module "thread_queue" {
-    source = "./sqs-with-backoff"
+    source = "github.com/fselcukcan/terraform-sqs-with-backoff.git"
     queue_name = "thread-queue"
     env = var.env
 }
